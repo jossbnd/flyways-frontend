192c4df30232710734f5b20c0eefe363a0a33f8b
pierreobeau <beauliver@gmail.com>
2022-10-25 14:47:37 +0200

    updated TopBar and created component ProfilModal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -17,7 +17,7 @@ export default function HomeScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
 });
