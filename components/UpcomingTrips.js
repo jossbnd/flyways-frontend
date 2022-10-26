f188691f72c72ef5af96847988aec2b5746406f1
Josselin Bonnaud <josselin.bonnaud@gmail.com>
2022-10-26 12:30:28 +0200

    style ProfileScreen

---
+++ components/UpcomingTrips.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+
+// Import des fonts
+import StyledRegularText from "../components/StyledBoldText";
+import StyledBoldText from "../components/StyledBoldText";
+
+export default function UpcomingTrips(props) {
+
+  return (
+  <TouchableOpacity style={styles.container}>
+    <View style={styles.descriptionText}>
+        <StyledBoldText title={props.arrival} style={{ fontSize: 24 }}/>
+        <StyledRegularText title={'Passengers: ' + props.passengers + '/' + props.capacity} style={{ fontSize: 12 }}/>
+    </View>
+    <View style={styles.dateContainer}>
+        <StyledBoldText title={props.date} style={{ fontSize: 18 }}/>
+        <StyledRegularText title={props.time} style={{ fontSize: 12 }}/>
+    </View>
+  </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 70,
+    backgroundColor: '#C9C9C9',
+    borderRadius: 5,
+    borderBottomWidth: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20
+  },
+  descriptionText: {
+    flex: 1,
+    width: '60%'
+  },
+  dateContainer: {
+    alignItems: 'flex-end'
+  }
+
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,41 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import TopBar from "../components/TopBar";
 
 // Import des fonts
 import StyledRegularText from "../components/StyledBoldText";
 import StyledBoldText from "../components/StyledBoldText";
+
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 // Import FontAwesome
 import FontAwesome from "react-native-vector-icons/FontAwesome";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+
+// Import composants upcoming trips
+import UpcomingTrips from "../components/UpcomingTrips";
+
+const BACK_END_ADDRESS = "https://flyways-backend.vercel.app/";
+
 
 export default function HomeScreen({ navigation }) {
 
+
+  const [upcomingTrips, setUpcomingTrips] = useState([]);
+  const [test, setTest] = useState(false);
+
+
   const user = useSelector((state) => state.user.value);
 
+  useEffect(() => {
+    fetch(`${BACK_END_ADDRESS}/users/info/${user.token}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.user.trips);
+    })
 
+  }, [test])
 
 
   return (
@@ -25,8 +46,22 @@ export default function HomeScreen({ navigation }) {
         <StyledBoldText title={user.firstName + ' ' + user.lastName} style={styles.userName} />
         <View style={styles.reviewContainer}>
           <FontAwesome name='star' size={20} style={{ marginRight: 5, color: '#f1c40f' }}/>
-          <StyledRegularText title='4.5 / 5 (26 reviews)' />
+          <StyledRegularText title='4.5 / 5 (26 reviews)' style={styles.reviews} />
         </View>
+        <TouchableOpacity style={styles.sarchButton} onPress={() => setTest(!test)}>
+          <StyledRegularText title='Search for a Trip' style={styles.textButton} />
+          <FontAwesome5 name='search' size={38} color='#ffffff' />
+        </TouchableOpacity>
+        <StyledRegularText title='Upcoming Trips' style={styles.upcomingTripsTitle} />
+        <View style={styles.upcomingTripsContainer}>
+          <UpcomingTrips arrival='Paris' passengers='2' capacity='5' date='02 Nov 22' time='9h30'  />
+          <UpcomingTrips />
+
+        </View>
+
+
+
+
       </View>
     </SafeAreaView>
   );
@@ -55,6 +90,33 @@ const styles = StyleSheet.create({
   reviewContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    marginTop: 5
+  },
+  reviews: {
+    fontSize: 12
+  },
+  sarchButton: {
+    height: 77,
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#1EA85F',
+    borderRadius: 5,
+    paddingHorizontal: 30,
+    marginTop: 30
+  },
+  textButton: {
+    color: '#ffffff',
+    fontSize: 26,
+  },
+  upcomingTripsTitle :{
+    fontSize: 22,
+    marginTop: 30,
+  },
+  upcomingTripsContainer: {
+    width: '90%',
+    marginTop: 10,
   }
 });
