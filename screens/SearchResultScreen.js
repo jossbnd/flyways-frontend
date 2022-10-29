e3ef8a62a87a0d74d1a9d4a7738cee2b9691e117
Kevin Dutier <kevin.dutier@gmail.com>
2022-10-29 13:43:35 +0200

    switched to vercel IP

--- screens/SearchResultScreen.js
+++ screens/SearchResultScreen.js
@@ -19,70 +19,12 @@ import SearchResultTrip from "../components/SearchResultTrip";
 import { faBlackTie } from "@fortawesome/free-brands-svg-icons";
 
 const BACK_END_ADDRESS = "https://flyways-backend.vercel.app/";
-const BACK_END_ADDRESS_LOCAL = "http://192.168.1.20:3000";
+const BACK_END_ADDRESS_LOCAL = "http://192.168.1.64:3000";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 export default function SearchResultScreen({ navigation, route: { params } }) {
-  // const placeHolderData = {
-  //   result: true,
-  //   sortedResult: [
-  //     {
-  //       tripFoundToken: "vxKhLCtBY-j-W291QB4oHR3WEDX0tZgM",
-  //       date: "2025-10-05T08:30:00.000Z",
-  //       passengers: [
-  //         {
-  //           isLeader: true,
-  //           passengerToken: "UoswI6AR19Q6vO5sbn4YvbWzj5uqlNn3",
-  //           firstName: "Jean",
-  //           lastName: "Valjean",
-  //           profilePicture:
-  //             "https://i.pinimg.com/564x/79/1c/ee/791cee75e44b3314a5445a456a4920ba.jpg",
-  //           rating: 4,
-  //           languagesSpoken: ["jp", "fr", "it"],
-  //           _id: "635a93b892810c68e64265cb",
-  //         },
-  //       ],
-  //       passengersNumber: 1,
-  //       capacity: 5,
-  //       arrivalCoords: {
-  //         latitude: 140,
-  //         longitude: 35,
-  //         description: "yes",
-  //         _id: "635a93b892810c68e64265cd",
-  //       },
-  //       distToDestination: 1.215616,
-  //     },
-  //     {
-  //       tripFoundToken: "IM9Uc5eTQPd8c1WJ25aHZlpGHbZP9VH0",
-  //       date: "2030-10-05T08:50:00.000Z",
-  //       passengers: [
-  //         {
-  //           isLeader: true,
-  //           passengerToken: "UoswI6AR19Q6vO5sbn4YvbWzj5uqlNn3",
-  //           firstName: "Font",
-  //           lastName: "Weesome",
-  //           profilePicture:
-  //             "https://i.kym-cdn.com/photos/images/original/001/809/674/f4a.jpg",
-  //           rating: 4,
-  //           languagesSpoken: ["de", "jp"],
-  //           _id: "635a93bd92810c68e64265d3",
-  //         },
-  //       ],
-  //       passengersNumber: 1,
-  //       capacity: 3,
-  //       arrivalCoords: {
-  //         latitude: 140,
-  //         longitude: 35,
-  //         description: "yes",
-  //         _id: "635a93bd92810c68e64265d5",
-  //       },
-  //       distToDestination: 0.433217501146,
-  //     },
-  //   ],
-  // };
-
   const [searchResult, setSearchResult] = useState(null);
 
   const handleFetch = () => {
@@ -104,7 +46,6 @@ export default function SearchResultScreen({ navigation, route: { params } }) {
             })
           );
         } else {
-          // TODO: display msg if no trip has been found
           setSearchResult(() => {
             return (
               <StyledRegularText title="No result found, would you like to create a new trip ?" />
